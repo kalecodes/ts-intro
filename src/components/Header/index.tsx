9af7c64d0e609ba8095058c9dd9550e24da61d9b
kalen510 <kalen.wiley@gmail.com>
2023-06-07 22:58:36 -0400

    union types and type aliases

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -16,16 +16,63 @@ function Header() {
 
   // type annotation for an object type
   // must include array type ([] alone results in error; interpreted as must be an empty array)
-  function printTask(task: { title: string, subtasks: string[] }) {
+  // if you don't specify a type for properties , it will be assumed to be 'any'
+  // ? after the property name denotes an optional property
+  function printTask(task: { title: string, subtasks?: string[] }) {
     console.log(task.title);
-    task.subtasks.forEach(subtask => console.log("- " + subtask));
+    if (task.subtasks !== undefined) {
+      task.subtasks.forEach(subtask => console.log("- " + subtask));
+    }
+    
   };
-
   printTask(
     {
       title: "Grocery Shopping",
       subtasks: ["Trader Joes", "Sam's Club", "Kroger"]
     }
+  )
+
+  // Union Type - formed from two or more other types (referred to as members)
+  // represent values that may be any one of those types
+  // typescript will only allow an operation if it is valid for every member of the union
+    // we cannot perform string or number specific operations in this function without first
+    // 'narrowing' the union (conditional statements to run different operations on each member)
+  function printAge(age: number | string) {
+    console.log("Your age is: " + age);
+  }
+  printAge(26);
+  printAge("twenty six");
+  printAge(26 + " years old")
+
+  // Type Aliases - a name for any type (to use the same type more than once and refer to it by a certain name)
+  type Task = {
+    title: string;
+    subtasks?: string[];
+  };
+  type Age = number | string;
+
+  function printInfo(task: Task, age: Age) {
+    console.log(task.title);
+    if (task.subtasks !== undefined) {
+      task.subtasks.forEach(subtask => {
+        console.log("-" + subtask);
+      });
+    }
+    console.log("You better hurry! You are already " + age + ", ... running low on time.")
+  }
+  printInfo(
+    {
+      title: "Grocery Shopping",
+      subtasks: ["Trader Joes", "Sam's Club", "Kroger"]
+    },
+    36
+  )
+  printInfo(
+    {
+      title: "Get the laundry done",
+      subtasks: []
+    },
+    "twenty nine"
   )
 
   return (
